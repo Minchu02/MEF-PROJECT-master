927050f2fdc3fd336506f858e91b16793190072b
Minchu02 <minchusatheesh@gmail.com>
2025-06-13 20:35:41 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// client/src/index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';        // Optional global styles
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
